ba1a7f0a54a58f54dae9212230cc9e5cb5f5ac0e
Harrison Cramer <harrisoncramer@gmail.com>
2022-10-30 16:35:53 -0400

    Added small comments to vite config

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue2'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   server: {
     proxy: {
       "/api/msg": {
-        target: "http://localhost:3000/msg",
+        target: "http://localhost:3000/msg", /* We're using json-server in development */
       },
     },
   },
+  /* Quirk of Vue 2.7 */
   resolve: { alias: { vue: 'vue/dist/vue.esm.js' } },
   test: {
     globals: true,
